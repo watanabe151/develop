402f918862c3c749c0d4ab30ef75b3b459512c34
watanabe1010 <watabase1010@outlook.jp>
2020-11-13 21:12:33 +0900

    Railsアプリケーションを作成。

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,30 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+require("test.js")
+
+
+import 'bootstrap';
+import '../stylesheets/application';
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+//初回読み込み、リロード、ページ切り替えで動く。
+//$(document).on('turbolinks:load', function() { });
+
+//初回読み込み、ページ切り替えで動く。リロードは動かない
+//$(document).on('turbolinks:render', function() {　});
+
+//ページ遷移前に行いたい処理用。ページ切り替えでもリロードでも動かない
+//$(document).on('turbolinks:request-start', function() {　});
---
+++ app/javascript/test.js
@@ -0,0 +1,5 @@
+$(function() {
+    console.log("OK");
+    $('#hoge').css('color','red');
+  });
+
